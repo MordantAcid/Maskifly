1ca4aa7e9daa4f8ac6bd6c80aa7085492ad01826
MordantAcid <cagej7517@gmail.com>
2026-05-18 15:54:54 +0300

    Create autograd.py

---
+++ maskinfly/autograd.py
@@ -0,0 +1,29 @@
+from typing import Callable, Any, Optional
+from contextlib import contextmanager
+
+class Context:
+    """Хранит функцию backward и сохранённые тензоры для операции."""
+    def __init__(self, backward_fn: Callable, saved_tensors: Any):
+        self.backward_fn = backward_fn
+        self.saved_tensors = saved_tensors
+    
+    def backward(self, ctx, grad):
+        return self.backward_fn(ctx, grad)
+
+
+_global_grad_enabled = True
+
+@contextmanager
+def no_grad():
+    """Контекстный менеджер, отключающий вычисление градиентов."""
+    global _global_grad_enabled
+    old_state = _global_grad_enabled
+    _global_grad_enabled = False
+    try:
+        yield
+    finally:
+        _global_grad_enabled = old_state
+
+
+def is_grad_enabled() -> bool:
+    return _global_grad_enabled
