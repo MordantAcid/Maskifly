aa1c21744350bc743452541573095193f58a7375
MordantAcid <cagej7517@gmail.com>
2026-05-20 20:47:58 +0300

    Update test_tensor.py

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -1,20 +1,150 @@
-import inspect
+import numpy as np
 import pytest
-from maskinfly.utils import find_variable_name, SENSITIVE_VAR_NAMES
+from numpy.testing import assert_allclose
+from maskinfly.tensor import Tensor
+from maskinfly.autograd import no_grad
 
-def test_sensitive_var_names():
-    assert "password" in SENSITIVE_VAR_NAMES
-    assert "token" in SENSITIVE_VAR_NAMES
-    assert "api_key" in SENSITIVE_VAR_NAMES
+def test_tensor_creation():
+    t1 = Tensor(5)
+    assert t1.data == np.array(5, dtype=np.float32)
+    assert t1.requires_grad is False
+    assert t1.grad is None
 
-def test_find_variable_name_from_local():
-    test_value = "secret123"
-    def inner():
-        return find_variable_name(test_value, frame_depth=2)
-    name = inner()
-    assert name == "test_value"
+    t2 = Tensor([1, 2, 3], requires_grad=True)
+    assert t2.shape == (3,)
+    assert t2.requires_grad is True
+    np.testing.assert_array_equal(t2.grad, np.zeros(3))
 
-def test_find_variable_name_not_found():
-    # Временный объект без имени
-    result = find_variable_name(object(), frame_depth=1)
-    assert result is None
+    with pytest.raises(TypeError):
+        Tensor("string")
+
+def test_tensor_properties():
+    t = Tensor([[1, 2], [3, 4]])
+    assert t.shape == (2, 2)
+    assert t.numpy().shape == (2, 2)
+    assert repr(t) == "Tensor([[1. 2.]\n [3. 4.]], requires_grad=False)"
+
+def test_arithmetic_operations():
+    a = Tensor(2.0, requires_grad=True)
+    b = Tensor(3.0, requires_grad=True)
+    c = a + b
+    assert c.data == 5.0
+    c.backward()
+    assert a.grad == 1.0
+    assert b.grad == 1.0
+
+    d = a * b
+    d.backward()
+    # градиенты накапливаются
+    assert a.grad == 1.0 + 3.0   # от сложения и умножения
+    assert b.grad == 1.0 + 2.0
+
+    # тест вычитания, деления, степени
+    a.zero_grad(); b.zero_grad()
+    e = a - b
+    e.backward()
+    assert a.grad == 1.0
+    assert b.grad == -1.0
+
+    a.zero_grad(); b.zero_grad()
+    f = a / b
+    f.backward()
+    assert a.grad == 1/3
+    assert b.grad == -2/9
+
+    a.zero_grad()
+    g = a ** 2
+    g.backward()
+    assert a.grad == 4.0
+
+def test_matmul():
+    a = Tensor([[1.0, 2.0]], requires_grad=True)   # (1,2)
+    b = Tensor([[3.0], [4.0]], requires_grad=True) # (2,1)
+    c = a.matmul(b)  # (1,1)
+    c.backward()
+    assert c.data == 11.0
+    assert a.grad is not None
+    assert b.grad is not None
+    np.testing.assert_array_almost_equal(a.grad, [[3.0, 4.0]])
+    np.testing.assert_array_almost_equal(b.grad, [[1.0], [2.0]])
+
+def test_sum():
+    a = Tensor([[1, 2], [3, 4]], requires_grad=True)
+    s = a.sum()
+    s.backward()
+    np.testing.assert_array_equal(a.grad, np.ones((2,2)))
+    a.zero_grad()
+    s_axis = a.sum(axis=0)
+    s_axis.backward(np.array([1,1]))
+    np.testing.assert_array_equal(a.grad, [[1,1],[1,1]])
+
+def test_reshape():
+    a = Tensor(np.arange(6), requires_grad=True)
+    b = a.reshape(2,3)
+    b.backward(np.ones((2,3)))
+    np.testing.assert_array_equal(a.grad, np.ones(6))
+
+def test_relu():
+    a = Tensor([-1, 0, 2], requires_grad=True)
+    r = a.relu()
+    r.backward(np.array([1,1,1]))
+    np.testing.assert_array_equal(r.data, [0,0,2])
+    np.testing.assert_array_equal(a.grad, [0,0,1])
+
+def test_exp_log():
+    a = Tensor(1.0, requires_grad=True)
+    e = a.exp()
+    e.backward()
+    assert a.grad is not None
+    # явное приведение ожидаемого значения к float32
+    expected_exp = np.exp(1.0, dtype=np.float32)
+    assert_allclose(a.grad, expected_exp, rtol=1e-5)
+
+    a.zero_grad()
+    l = a.log()
+    l.backward()
+    assert a.grad is not None
+    expected_log = 1.0 / (1.0 + 1e-8)
+    assert_allclose(a.grad, np.float32(expected_log), rtol=1e-5)
+
+def test_mean():
+    a = Tensor([1.0, 2.0, 3.0, 4.0], requires_grad=True)
+    m = a.mean()
+    m.backward()
+    assert a.grad is not None
+    np.testing.assert_array_almost_equal(a.grad, [0.25,0.25,0.25,0.25])
+
+def test_stack():
+    a = Tensor([1,2], requires_grad=True)
+    b = Tensor([3,4], requires_grad=True)
+    s = Tensor.stack([a,b], axis=0)
+    assert s.shape == (2,2)
+    s.sum().backward()
+    np.testing.assert_array_equal(a.grad, [1,1])
+    np.testing.assert_array_equal(b.grad, [1,1])
+
+def test_backward_complex_graph():
+    x = Tensor(2.0, requires_grad=True)
+    y = x ** 2
+    z = y + x
+    z.backward()
+    # dz/dx = 2*x + 1 = 5
+    assert x.grad == 5.0
+
+def test_zero_grad():
+    t = Tensor(5.0, requires_grad=True)
+    (t**2).backward()
+    assert t.grad == 10.0
+    t.zero_grad()
+    assert t.grad == 0.0
+
+def test_no_grad_on_operations():
+    """Проверяет, что внутри no_grad градиенты не вычисляются и тензоры не требуют градиентов."""
+    with no_grad():
+        a = Tensor(2.0, requires_grad=True)
+        b = a ** 2
+    assert b.requires_grad is False
+    assert b._ctx is None
+    # Вызов backward не должен ничего менять, т.к. графа нет
+    b.backward()  # не должно быть эффекта
+    assert a.grad is None or np.all(a.grad == 0)  # градиент не изменился
