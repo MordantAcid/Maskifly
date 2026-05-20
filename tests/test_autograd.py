3a0b5b0f642da258b50751d4430779b61462a988
MordantAcid <cagej7517@gmail.com>
2026-05-20 19:08:28 +0300

    Update test_autograd.py

--- tests/test_autograd.py
+++ tests/test_autograd.py
@@ -3,15 +3,19 @@ from maskinfly.autograd import no_grad, is_grad_enabled
 from maskinfly.tensor import Tensor
 
 def test_no_grad_context():
-    """Проверяет, что внутри контекста no_grad флаг is_grad_enabled выключен."""
     assert is_grad_enabled() is True
     with no_grad():
         assert is_grad_enabled() is False
     assert is_grad_enabled() is True
 
-def test_no_grad_does_not_affect_tensor_creation():
-    """В текущей реализации флаг _global_grad_enabled не используется в Tensor,
-    поэтому тензор с requires_grad=True всё равно создаётся с градиентами."""
+def test_no_grad_disables_gradient_graph():
+    a = Tensor(2.0, requires_grad=True)
+    with no_grad():
+        b = a + 3
+    assert b.requires_grad is False, "Операция внутри no_grad не должна требовать градиентов"
+    assert b._ctx is None, "Граф операций не должен строиться"
+
+def test_no_grad_allows_creation_of_requires_grad_tensor():
     with no_grad():
         t = Tensor([1.0, 2.0], requires_grad=True)
     assert t.requires_grad is True
