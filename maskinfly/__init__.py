c080f134f5683ed7b94c3d5fa11b6ac9ac360557
MordantAcid <cagej7517@gmail.com>
2026-05-20 17:45:45 +0300

    Update __init__.py

--- maskinfly/__init__.py
+++ maskinfly/__init__.py
@@ -21,10 +21,10 @@ def mask(data,
          mask_char: str = "*",
          mask_length: int = 3,
          custom_patterns: Optional[Dict[str, Tuple[Pattern, Callable]]] = None,
-         # Новые параметры
          audit_format: str = 'text',
          audit_custom_handler: Optional[Callable[[Dict[str, Any]], None]] = None,
-         audit_app_name: Optional[str] = None):
+         audit_app_name: Optional[str] = None,
+         deep_mask: bool = False):   # новый параметр
     masker = Masker(
         audit_enabled=audit_enabled,
         audit_logger=audit_logger,
@@ -34,6 +34,7 @@ def mask(data,
         custom_patterns=custom_patterns,
         audit_format=audit_format,
         audit_custom_handler=audit_custom_handler,
-        audit_app_name=audit_app_name
+        audit_app_name=audit_app_name,
+        deep_mask=deep_mask          # передаём
     )
     return masker.mask(data, var_name=var_name)
