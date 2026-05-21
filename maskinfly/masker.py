d0e7cf189eaf7c9b42f818bb10fab33a2601e2c0
MordantAcid <cagej7517@gmail.com>
2026-05-21 19:50:28 +0300

    Update masker.py

--- maskinfly/masker.py
+++ maskinfly/masker.py
@@ -2,8 +2,9 @@ import re
 import json
 import yaml
 
-from typing import Any, Dict, List, Tuple, Optional, Union, Callable
+from typing import Any, Dict, List, Tuple, Optional, Union, Callable, Pattern
 from collections.abc import Mapping as MappingABC, Sequence as SequenceABC
+
 from maskinfly.patterns import PATTERNS, DEFAULT_MASK_CHAR, DEFAULT_MASK_LENGTH, full_mask_replacer
 from maskinfly.audit import AuditLogger
 from maskinfly.utils import find_variable_name, SENSITIVE_VAR_NAMES
