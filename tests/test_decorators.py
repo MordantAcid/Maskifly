890b5b34953c76e43f86831444825d1bc77cb3d1
MordantAcid <cagej7517@gmail.com>
2026-05-21 19:54:25 +0300

    Create test_decorators.py

---
+++ tests/test_decorators.py
@@ -0,0 +1,16 @@
+import pytest
+from maskinfly import mask_output
+
+def test_mask_output_decorator_sync():
+    @mask_output()
+    def func():
+        return {"password": "secret"}
+    assert func() == {"password": "***"}
+
+@pytest.mark.asyncio
+async def test_mask_output_decorator_async():
+    @mask_output(mask_char='X', mask_length=2)
+    async def func():
+        return "token=abc"
+    result = await func()
+    assert result == "token=XX"
