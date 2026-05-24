294dd09785b6d6d436532538a59456d43873083d
MordantAcid <cagej7517@gmail.com>
2026-05-24 17:38:13 +0300

    Update test_decorators.py

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,5 +1,4 @@
 import pytest
-
 from maskinfly import mask_output
 
 def test_mask_output_decorator_sync():
@@ -12,6 +11,6 @@ def test_mask_output_decorator_sync():
 async def test_mask_output_decorator_async():
     @mask_output(mask_char='X', mask_length=2)
     async def func():
-        return "token=abc"
+        return {"token": "abc"}
     result = await func()
-    assert result == "token=XX"
+    assert result == {"token": "XX"}
