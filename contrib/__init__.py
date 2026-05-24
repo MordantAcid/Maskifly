1c5e89fac4845038df6cf311290a8b06e6f1ecd7
MordantAcid <cagej7517@gmail.com>
2026-05-24 19:00:36 +0300

    Create __init__.py

---
+++ contrib/__init__.py
@@ -0,0 +1,28 @@
+"""maskinfly.contrib - интеграция с веб-фреймворками.
+
+Представляет middleware и утилиты для Django и FastApi.
+"""
+
+from.django import (
+    get_masker_from_settings,
+    MaskingMiddleware,
+    apply_mask_to_request,
+)
+from.fastapi import (
+    MaskResponseMiddleware,
+    MaskResponseDependency,
+    mask_response,
+    setup_fastapi_masking,
+    DEFAULT_MASKER,
+)
+
+__all__ = [
+    "get_masker_from_settings",
+    "MaskingMiddleware",
+    "apply_mask_to_request",
+    "MaskResponseMiddleware",
+    "MaskResponseDependency",
+    "mask_response",
+    "setup_fastapi_masking",
+    "DEFAULT_MASKER",
+]
